9d47ba5949053e008f22dc7c237d4df2c9b297e1
ganchau <gancchau@gmail.com>
2015-09-10 14:05:54 -0400

    updated to iOS 9 with updated plist to fix app transport security issue

---
+++ Weather/WeatherHTTPClient.h
@@ -0,0 +1,13 @@
+//
+//  WeatherHTTPClient.h
+//  Weather
+//
+//  Created by Gan Chau on 9/9/15.
+//  Copyright (c) 2015 Scott Sherwood. All rights reserved.
+//
+
+#import "AFHTTPSessionManager.h"
+
+@interface WeatherHTTPClient : AFHTTPSessionManager
+
+@end
---
+++ Weather/WeatherHTTPClient.m
@@ -0,0 +1,13 @@
+//
+//  WeatherHTTPClient.m
+//  Weather
+//
+//  Created by Gan Chau on 9/9/15.
+//  Copyright (c) 2015 Scott Sherwood. All rights reserved.
+//
+
+#import "WeatherHTTPClient.h"
+
+@implementation WeatherHTTPClient
+
+@end
